a2d05a07897888eacfa93c318df59d585fe364b4
Ekaterina Cherednik <kinder_che@mail.ru>
2019-01-06 01:48:33 +0700

    Тест на проверку расстояния

---
+++ Sandbox/src/test/java/ru/stqa1/pft/sanbox/PointTests.java
@@ -0,0 +1,17 @@
+
+package ru.stqa1.pft.sanbox;
+
+import org.testng.annotations.Test;
+import ru.stqa1.pft.sandbox.Point;
+
+public class PointTests {
+
+  @Test
+  public void testArea(){
+  Point p = new Point(1,2, 3,4);
+
+ assert p.distance()==2.8284271247461903 ;
+
+
+  }
+}
