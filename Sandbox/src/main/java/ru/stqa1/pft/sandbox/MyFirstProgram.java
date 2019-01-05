1509c66bdc6f51ef9f09e1bf0f8885e7a98c3cd5
Ekaterina Cherednik <kinder_che@mail.ru>
2019-01-06 01:47:25 +0700

    Решение задачи про расстояние, пункт 4.

--- Sandbox/src/main/java/ru/stqa1/pft/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/stqa1/pft/sandbox/MyFirstProgram.java
@@ -5,13 +5,12 @@ public class MyFirstProgram {
   public static void main(String[] args) {
 
 
-    Point p1 = new Point(1, 2);
-    Point p2 = new Point(3, 4);
-    System.out.println("Расстояние между точками равно" + distance(p1, p2));
+    Point p = new Point(1,2, 3, 4);
+    System.out.println("Расстояние между точками с координатами "  +p.a +";" +p.b + " и " + p.c +";" +p.d + " равно " + p.distance());
   }
 
 
-    public static double distance (Point p1, Point p2){
-      return Math.sqrt(((p2.a - p1.a) * (p2.a - p1.a)) + ((p2.b - p1.b) * (p2.b - p1.b)));
-    }
+   /*public static double distance (Point p){
+      return Math.sqrt(((p.a - p.a) * (p.a - p.a)) + ((p.b - p.b) * (p.b - p.b)));
+    }*/
   }
--- Sandbox/src/main/java/ru/stqa1/pft/sandbox/Point.java
+++ Sandbox/src/main/java/ru/stqa1/pft/sandbox/Point.java
@@ -3,12 +3,19 @@ package ru.stqa1.pft.sandbox;
 public class Point {
   public double a;
   public double b;
+  public double c;
+  public double d;
 
-  public Point (double a, double b){
-    this.a=a;
-    this.b=b;
+  public Point(double a, double b, double c, double d) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    this.d = d;
 
   }
 
+  public double distance(){
+    return Math.sqrt(((this.c - this.a) * (this.c - this.a)) + ((this.d - this.b) * (this.d - this.b)));
+  }
 }
 
