239abce4d04d5217f3d98be0f497837ebe94a73b
Ekaterina Cherednik <kinder_che@mail.ru>
2019-01-06 21:49:13 +0700

    Выделены методы

---
+++ addressbook_web_tests/src/main/java/ru/stqa1/pft/addressbook/AddressBookMethods.java
@@ -0,0 +1,22 @@
+package ru.stqa1.pft.addressbook;
+
+public class AddressBookMethods {
+
+  public static void main(String args[]){
+    click();
+    clear();
+    sendKeys();
+  }
+
+  public static void click(){
+
+  }
+
+  public static void clear(){
+
+  }
+
+  public static void sendKeys(){
+
+  }
+}
