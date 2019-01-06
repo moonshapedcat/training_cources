9eb2b30779ad9bf80323b24cd43f0dde76954d24
Ekaterina Cherednik <kinder_che@mail.ru>
2019-01-06 23:28:08 +0700

    Добавлен класс GroupData

--- addressbook_web_tests/src/main/java/ru/stqa1/pft/addressbook/AddressBookMethods.java
+++ addressbook_web_tests/src/main/java/ru/stqa1/pft/addressbook/AddressBookMethods.java
@@ -2,21 +2,29 @@ package ru.stqa1.pft.addressbook;
 
 public class AddressBookMethods {
 
-  public static void main(String args[]){
+  public static void main(String args[]) {
     click();
     clear();
-    sendKeys();
+
+    GroupData info = new GroupData("Some text");
+    sendKeys(info);
   }
 
-  public static void click(){
+
+  public static void click() {
 
   }
 
-  public static void clear(){
+  public static void clear() {
 
   }
 
-  public static void sendKeys(){
+  public static String sendKeys(GroupData info) {
+    System.out.println(info);
+    String s = "Information has been added";
+    return s;
+
 
   }
 }
+
---
+++ addressbook_web_tests/src/main/java/ru/stqa1/pft/addressbook/GroupData.java
@@ -0,0 +1,18 @@
+package ru.stqa1.pft.addressbook;
+
+ public class GroupData {
+   String info;
+
+
+  public GroupData(String info) {
+     this.info = "text";
+   }
+ }
+
+  /*public String sendKeys() {
+    System.out.println(info);
+    return(info);
+}
+
+
+*/
