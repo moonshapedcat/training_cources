706a1e429f566bc395792dc011bdaa9b984e762a
Ekaterina Cherednik <kinder_che@mail.ru>
2019-01-12 17:59:36 +0700

    Извлечен и параметризован метод заполнения формы

---
+++ addressbook_web_tests/src/test/java/ru/stqa1/pft/addressbook/ContactData.java
@@ -0,0 +1,43 @@
+package ru.stqa1.pft.addressbook;
+
+public class ContactData {
+  private final String yourName;
+  private final String yourMidlename;
+  private final String yourLastname;
+  private final String yourNickname;
+  private final String yourTitle;
+  private final String yourCompanyName;
+
+  public ContactData(String yourName, String yourMidlename, String yourLastname, String yourNickname, String yourTitle, String yourCompanyName) {
+    this.yourName = yourName;
+    this.yourMidlename = yourMidlename;
+    this.yourLastname = yourLastname;
+    this.yourNickname = yourNickname;
+    this.yourTitle = yourTitle;
+    this.yourCompanyName = yourCompanyName;
+  }
+
+  public String getYourName() {
+    return yourName;
+  }
+
+  public String getYourMidlename() {
+    return yourMidlename;
+  }
+
+  public String getYourLastname() {
+    return yourLastname;
+  }
+
+  public String getYourNickname() {
+    return yourNickname;
+  }
+
+  public String getYourTitle() {
+    return yourTitle;
+  }
+
+  public String getYourCompanyName() {
+    return yourCompanyName;
+  }
+}
