74525c5f2d03b42e008a05e8335b2934e8eecc7a
Ekaterina Cherednik <kinder_che@mail.ru>
2019-01-20 19:39:10 +0700

    +class initGroupModification

--- addressbook_web_tests/src/test/java/ru/stqa1/pft/addressbook/appmanager/Grouphelper.java
+++ addressbook_web_tests/src/test/java/ru/stqa1/pft/addressbook/appmanager/Grouphelper.java
@@ -38,6 +38,10 @@ public class GroupHelper extends HelperBase{
   }
 
   public void initGroupModification() {
-    click(By.name("selected[]"));
+    click(By.name("edit"));
+  }
+
+  public void submitGroupModification() {
+    click(By.name("update"));
   }
 }
