0fb2d4e491380c2d68934cb44901d438e52ab9dd
Ekaterina Cherednik <kinder_che@mail.ru>
2019-02-28 23:38:09 +0700

    Задание 12 и 13: группы и контакты до 6.5

--- addressbook_web_tests/src/test/java/ru/stqa1/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook_web_tests/src/test/java/ru/stqa1/pft/addressbook/tests/ContactCreationTests.java
@@ -1,21 +1,40 @@
 package ru.stqa1.pft.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa1.pft.addressbook.model.ContactData;
 import ru.stqa1.pft.addressbook.model.Contacts;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase{
 
-  @Test
-  public void testContactCreation() throws Exception {
-    Contacts before = app.contact().all();
-    ContactData contactData = new ContactData().withName("Ivan").withMiddlename("Ivanovich").withLastname("Ivanov").withNickname("Vanya").withTitle("MyTitle").withCompanyName("MyCompany");
-    app.contact().create(contactData);
-    Contacts after = app.contact().all();
-    assertThat(after.size(),equalTo(before.size() +1));
-    assertThat(after, equalTo(before.withAdded(contactData.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
+  @DataProvider
+  public Iterator<Object[]> validContacts() throws FileNotFoundException {
+    List<Object[]> list =  new ArrayList<Object[]>();
+    list.add(new Object[] {new ContactData().withName("name 1").withMiddlename("middlename1").withLastname("lastname1")
+            .withNickname("nickname1").withCompanyName("companyName1").withTitle("title1")});
+    list.add(new Object[] {new ContactData().withName("name 2").withMiddlename("middlename1").withLastname("lastname1")
+            .withNickname("nickname1").withCompanyName("companyName1").withTitle("title1")});
+    list.add(new Object[] {new ContactData().withName("name 1").withMiddlename("middlename1").withLastname("lastname1")
+            .withNickname("nickname1").withCompanyName("companyName1").withTitle("title1")});
+    return list.iterator();
   }
+
+  @Test(dataProvider = "validContacts")
+  public void testContactCreation(ContactData contactData) throws Exception {
+    String[] names = new String[] {"test1", "test2", "test3"};
+      Contacts before = app.contact().all();
+      app.contact().create(contactData);
+      Contacts after = app.contact().all();
+      assertThat(after.size(),equalTo(before.size() +1));
+      assertThat(after, equalTo(before.withAdded(contactData.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
+    }
+
 }
