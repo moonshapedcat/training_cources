a32d007eccd004929a7a88a9199c96b548f23396
Ekaterina Cherednik <kinder_che@mail.ru>
2019-03-03 13:26:34 +0700

    Задача 15, null pointer exeption

---
+++ addressbook_web_tests/src/test/java/ru/stqa1/pft/addressbook/tests/HbConnectionTest.java
@@ -0,0 +1,45 @@
+package ru.stqa1.pft.addressbook.tests;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import ru.stqa1.pft.addressbook.model.GroupData;
+
+import java.util.List;
+
+
+public class HbConnectionTest {
+
+ private static SessionFactory sessionFactory;
+
+  @BeforeClass
+ protected void setUp() throws Exception {
+
+  final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+            .configure()
+            .build();
+    try {
+      sessionFactory = new MetadataSources( registry ).buildMetadata().buildSessionFactory();
+    }
+    catch (Exception e) {
+      e.printStackTrace();
+      StandardServiceRegistryBuilder.destroy( registry );
+    }
+  }
+
+  @Test
+  public void TestHbConnection (){
+    Session session = sessionFactory.openSession();
+    session.beginTransaction();
+    List<GroupData> result = session.createQuery( "from GroupData" ).list();
+    for ( GroupData group : result ) {
+      System.out.println(group);
+    }
+    session.getTransaction().commit();
+    session.close();
+  }
+}
