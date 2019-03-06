3f46cca3382cf063aa2ed37501969060e3e8a17c
Ekaterina Cherednik <kinder_che@mail.ru>
2019-03-06 20:41:31 +0700

    Исправлен тест модификации контактов (задания 9 и 10)

--- addressbook_web_tests/src/test/java/ru/stqa1/pft/addressbook/tests/ContactModificationTest.java
+++ addressbook_web_tests/src/test/java/ru/stqa1/pft/addressbook/tests/ContactModificationTest.java
@@ -1,15 +1,9 @@
 package ru.stqa1.pft.addressbook.tests;
 
-import org.hamcrest.CoreMatchers;
-import org.hamcrest.MatcherAssert;
-import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa1.pft.addressbook.model.ContactData;
 import ru.stqa1.pft.addressbook.model.Contacts;
-
-import java.util.Set;
-import java.util.regex.Matcher;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -27,12 +21,15 @@ public class ContactModificationTest extends TestBase{
   public void ContactModificationTest() {
     Contacts before = app.contact().all();
     ContactData modifiedContact = before.iterator().next();
+
+
     ContactData contactData = new ContactData()
             .withId(modifiedContact.getId()).withName("Ivan").withMiddlename("Ivanovich").withLastname("Ivanov").withNickname("Vanya").withTitle("MyTitle").withCompanyName("MyCompany");
     app.contact().modify(contactData);
     Contacts after = app.contact().all();
     assertEquals(after.size(), before.size());
     assertThat(after, equalTo(before.without(modifiedContact).withAdded(contactData)));
+
   }
 
 }
